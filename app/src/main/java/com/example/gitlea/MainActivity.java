3b6ee35e116afeeac16816dc2defcdfc67300698
nlokantha <74893580+nlokantha@users.noreply.github.com>
2023-12-13 13:38:29 +0530

    Add TextView

--- app/src/main/java/com/example/gitlea/MainActivity.java
+++ app/src/main/java/com/example/gitlea/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.gitlea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView edit_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        edit_name=findViewById(R.id.text_name);
 
 
     }
